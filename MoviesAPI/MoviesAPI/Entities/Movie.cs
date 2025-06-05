1b69eaeadfab6a846b7036d5c08a938cd5b0c9c4
GBMaO <202501637264@alunos.estacio.br>
2025-06-05 14:31:12 -0300

    add movie class

---
+++ MoviesAPI/MoviesAPI/Entities/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength (40)]
+        public required string Name { get; set; }
+
+        [Required]
+        [Range(1,100)]
+        public decimal Price { get; set; }
+
+        public DateOnly ReleaseDAte { get; set; }
+    }
+}
